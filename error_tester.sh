6742d1e46448536ca4a77e3d2261dcbba7a56823
yshimazu <shimazuyuki7@gmail.com>
2022-03-16 11:45:59 +0900

    delete leak tester

--- error_tester.sh
+++ error_tester.sh
@@ -11,31 +11,11 @@ WHITE="\033[37m"
 RESET="\033[0m"
 
 testlog=test.log
-leaklog=leak.log
-tmplog=tmp.log
-
-check_leak() {
-	grep "All heap blocks were freed" $tmplog > /dev/null
-	if [ $? -eq 1 ]; then
-		printf $RED"Leak Detected!!\n"$RESET
-		echo "$1"
-	fi
-}
 
 store_leaklog() {
 	echo "file name: $1" >> $leaklog
 	cat $tmplog >> $leaklog
 	echo >> $leaklog
-}
-
-print_usage() {
-	echo "Usage:"
-	echo "./error_teseter.sh [no args and no option]"
-	echo "	check error-maps simply."
-	echo "	result is test.log"
-	echo "./error_teseter.sh --leak [no args]"
-	echo "	check error-maps using valgrind"
-	echo "	result is leak.log"
 }
 
 ##### Main Process #####
@@ -48,13 +28,5 @@ if [ $# -eq 0 ]; then
 		./cub3D $testmap 2>> $testlog
 		echo >> $testlog
 	done
-elif [ "$1" = "--leak" ]; then
-	for testmap in ./map/error/*; do
-		valgrind --leak-check=full --show-leak-kinds=all ./cub3D $testmap 2> $tmplog
-		check_leak $testmap
-		store_leaklog $testmap
-	done
-else
-	print_usage
 fi
 rm -f tmp.log
